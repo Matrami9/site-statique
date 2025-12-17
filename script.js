fbbdbbd8d5d883cc50aafc9afa06428f90a6ed0d
Matrami Mhadjou <matrami.mhadjou@epitech.eu>
2025-12-17 16:39:57 +0100

    v2.0

--- script.js
+++
@@ -1,6 +0,0 @@
-let burger = document.querySelector("#burger-menu")
-let button = document.querySelector("#button-bar");
-
-burger.addEventListener('click', () => {
-    button.classList.toggle('active')
-})
